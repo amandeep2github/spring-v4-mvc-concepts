2bc96a92848a9849674429900521eaa424688a5a
Aman <amandeepsingh@AmandeeinghsMBP.Home>
2016-06-10 00:20:16 +0530

    first check for hello world

---
+++ src/test/java/learn/spring/web/SpringMvcConceptsApplicationTests.java
@@ -0,0 +1,20 @@
+package learn.spring.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import learn.spring.web.Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class SpringMvcConceptsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
